f5d149c51ba7b35ae5e5840fe0f477aa119ffc79
Dmitry Komzolov <orlchejedaxdodg@rambler.ru>
2022-11-26 14:52:29 +0300

    prettier config reworked

--- src/components/Hotels/HotelItem.jsx
+++ src/components/Hotels/HotelItem.jsx
@@ -3,26 +3,26 @@ import PropTypes from 'prop-types';
 import Hotels from './Hotels';
 import './Hotels.css';
 
-function HotelItem({item}) {
-    return (
-        <div className="col-3 col-ss-3 homes-content-wrap">
-            <img className="homes-img" src={item.imageUrl} alt=""/>
-            <div className="homes-item">{item.name}</div>
-            <div className="homes-item-location">
-                {item.city},{item.country}
-            </div>
-        </div>
-    );
+function HotelItem({ item }) {
+ return (
+  <div className="col-3 col-ss-3 homes-content-wrap">
+   <img className="homes-img" src={item.imageUrl} alt="" />
+   <div className="homes-item">{item.name}</div>
+   <div className="homes-item-location">
+    {item.city},{item.country}
+   </div>
+  </div>
+ );
 }
 
 HotelItem.propTypes = {
-    item: PropTypes.shape({
-        id: PropTypes.string,
-        name: PropTypes.string,
-        city: PropTypes.string,
-        country: PropTypes.string,
-        imageUrl: PropTypes.string
-    }).isRequired
+ item: PropTypes.shape({
+  id: PropTypes.string,
+  name: PropTypes.string,
+  city: PropTypes.string,
+  country: PropTypes.string,
+  imageUrl: PropTypes.string
+ }).isRequired
 };
 
 export default HotelItem;
--- webpack.config.js
+++ webpack.config.js
@@ -5,39 +5,39 @@ const outputPath = path.resolve(__dirname, './dist');
 const entryPath = './src/index.js';
 
 module.exports = {
-    entry: entryPath,
-    output: {
-        path: outputPath,
-        filename: 'bundle[hash].js',
-    },
-    resolve: {
-        extensions: ['.js', '.jsx'],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: 'babel-loader',
-            },
-            {
-                test: /\.html$/,
-                use: 'html-loader',
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: ['file-loader'],
-            },
-        ],
-    },
-    plugins: [
-        new HTMLWebpackPlugin({
-            inject: 'body',
-            template: './public/index.html',
-        }),
-    ],
+ entry: entryPath,
+ output: {
+  path: outputPath,
+  filename: 'bundle[hash].js'
+ },
+ resolve: {
+  extensions: ['.js', '.jsx']
+ },
+ module: {
+  rules: [
+   {
+    test: /\.(js|jsx)$/,
+    exclude: /node_modules/,
+    use: 'babel-loader'
+   },
+   {
+    test: /\.html$/,
+    use: 'html-loader'
+   },
+   {
+    test: /\.css$/,
+    use: ['style-loader', 'css-loader']
+   },
+   {
+    test: /\.(png|svg|jpg|gif)$/,
+    use: ['file-loader']
+   }
+  ]
+ },
+ plugins: [
+  new HTMLWebpackPlugin({
+   inject: 'body',
+   template: './public/index.html'
+  })
+ ]
 };
