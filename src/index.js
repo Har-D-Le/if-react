39db77ca905b116d69b6401d8917348046b7d8cb
Dmitry Komzolov <orlchejedaxdodg@rambler.ru>
2022-11-27 16:52:19 +0300

    add Header component eslint config reworked

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App/App';
+import Header from './components/Header/Header';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
- <React.StrictMode>
-  <App />
- </React.StrictMode>
+  <React.StrictMode>
+    <Header />
+    <App />
+  </React.StrictMode>
 );
